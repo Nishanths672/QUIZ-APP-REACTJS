428931858ef361337ade88ed8a81ebc76858b26a
Nishanths672 <nishanths672@gmail.com>
2024-07-22 11:55:05 +0530

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,47 @@
+// src/Login.js
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validUsername = 'nishanth';
+    const validPassword = 'nishanth123';
+
+    if (username === validUsername && password === validPassword) {
+      onLogin();
+    } else {
+      window.alert('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
